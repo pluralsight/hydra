4869ef9b0df1c70e8c3bddbf950f36b9ed99f83c
Alex Silva <alex-silva@pluralsight.com>
2017-08-22 21:54:41 -0700

    unit tests

--- core/src/main/scala/hydra/core/ingest/Ingestor.scala
+++ core/src/main/scala/hydra/core/ingest/Ingestor.scala
@@ -1,62 +1,18 @@
 package hydra.core.ingest
 
-import akka.actor.{Actor, ActorRef, OneForOneStrategy, ReceiveTimeout, Stash, SupervisorStrategy}
-import akka.pattern.pipe
-import hydra.common.config.ActorConfigSupport
-import hydra.common.logging.LoggingAdapter
-import hydra.core.HydraException
-import hydra.core.akka.ComposingReceive
-import hydra.core.ingest.Ingestor.{IngestorInitializationError, IngestorInitialized}
+import akka.actor.{Actor, OneForOneStrategy, SupervisorStrategy}
+import hydra.core.akka.InitializingActor
 import hydra.core.protocol._
 
-import scala.concurrent.Future
-import scala.concurrent.duration.{Duration, _}
+import scala.concurrent.duration._
 
 /**
   * Created by alexsilva on 12/1/15.
   */
 
-trait Ingestor extends Actor with ActorConfigSupport with LoggingAdapter with ComposingReceive with Stash {
+trait Ingestor extends InitializingActor {
 
-  protected val initTimeout = 2.seconds //the initialization timeout
-
-  context.setReceiveTimeout(initTimeout)
-
-  override def receive: Receive = waitingForInitialization
-
-  /**
-    * This method is called upon initialization.  It should return one of the ingestor initialization messages:
-    * `IngestorInitialized` or `IngestorInitializationError`
-    *
-    * The default implementation returns IngestorInitialized.
-    */
-  def initIngestor: Future[HydraMessage] = Future.successful(IngestorInitialized)
-
-  override def preStart(): Unit = {
-    implicit val ec = context.dispatcher
-    pipe(initIngestor) to self
-  }
-
-  def waitingForInitialization: Receive = {
-    case Ingestor.IngestorInitialized =>
-      cancelReceiveTimeout
-      context.become(composedReceive)
-      log.info("Ingestor {}[{}] initialized", Seq(thisActorName, self.path): _*)
-      unstashAll()
-
-    case err@Ingestor.IngestorInitializationError(ex) =>
-      log.error(ex.getMessage)
-      initError(err)
-
-    case ReceiveTimeout =>
-      log.error(s"Ingestor $thisActorName[${self.path}] did not initialize in $initTimeout")
-      val err = IngestorInitializationException(self, s"Ingestor did not initialize in $initTimeout.")
-      initError(IngestorInitializationError(err))
-
-    case msg =>
-      log.debug(s"$thisActorName received message $msg while not initialized; stashing.")
-      stash()
-  }
+  override def initTimeout = 2.seconds
 
   override val baseReceive: Receive = {
     case Publish(_) =>
@@ -70,19 +26,11 @@ trait Ingestor extends Actor with ActorConfigSupport with LoggingAdapter with Co
       supervisor ! error.map(IngestorError(_)).getOrElse(IngestorCompleted)
   }
 
-  private def initError(err: IngestorInitializationError) = {
-    cancelReceiveTimeout
-    unstashAll()
-    context.system.eventStream.publish(err)
-    context.become(initializationError(err.t))
-  }
-
-  private def initializationError(ex: Throwable): Receive = {
+  override def initializationError(ex: Throwable): Receive = {
     case _ =>
       sender ! IngestorError(ex)
       ingestionError(ex)
   }
-
 
   def ingest(next: Actor.Receive) = compose(next)
 
@@ -104,38 +52,5 @@ trait Ingestor extends Actor with ActorConfigSupport with LoggingAdapter with Co
     log.error(s"Ingestion error: $error")
   }
 
-  private def cancelReceiveTimeout = {
-    context.setReceiveTimeout(Duration.Undefined)
-    unstashAll()
-  }
-
   override val supervisorStrategy = OneForOneStrategy() { case _ => SupervisorStrategy.Restart }
-}
-
-object Ingestor {
-
-  /**
-    * Signals this ingestor when to switch away from the `init` receive.
-    */
-  case object IngestorInitialized extends HydraMessage
-
-  case class IngestorInitializationError(t: Throwable) extends HydraMessage
-
-}
-
-@SerialVersionUID(1L)
-class IngestorInitializationException(ingestor: ActorRef, message: String, cause: Throwable)
-  extends HydraException(IngestorInitializationException.enrichedMessage(ingestor, message), cause) {
-  def getActor: ActorRef = ingestor
-}
-
-object IngestorInitializationException {
-  private def enrichedMessage(actor: ActorRef, message: String) =
-    Option(actor).map(a => s"${a.path}: $message").getOrElse(message)
-
-  private[ingest] def apply(actor: ActorRef, message: String, cause: Throwable = null) =
-    new IngestorInitializationException(actor, message, cause)
-
-  def unapply(ex: IngestorInitializationException): Option[(ActorRef, String, Throwable)] =
-    Some((ex.getActor, ex.getMessage, ex.getCause))
 }
--- core/src/test/scala/hydra/core/ingest/IngestorSpec.scala
+++ core/src/test/scala/hydra/core/ingest/IngestorSpec.scala
@@ -2,7 +2,8 @@ package hydra.core.ingest
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
-import hydra.core.ingest.Ingestor.{IngestorInitializationError, IngestorInitialized}
+import hydra.core.akka.ActorInitializationException
+import hydra.core.akka.InitializingActor.{InitializationError, Initialized}
 import hydra.core.protocol._
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
 
@@ -21,7 +22,7 @@ class IngestorSpec extends TestKit(ActorSystem("test")) with Matchers with FunSp
 
       expectMsgPF() {
         case i: IngestorError =>
-          i.error shouldBe a[IngestorInitializationException]
+          i.error shouldBe a[ActorInitializationException]
       }
 
       system.actorOf(Props(classOf[TestIngestor], true, false)) ! "hello"
@@ -34,16 +35,26 @@ class IngestorSpec extends TestKit(ActorSystem("test")) with Matchers with FunSp
       ct ! "hello"
       expectMsgPF(max = 5.seconds) {
         case i: IngestorError =>
-          i.error shouldBe a[IngestorInitializationException]
-          i.error.asInstanceOf[IngestorInitializationException].getActor.path shouldBe ct.path
-          IngestorInitializationException
-            .unapply(i.error.asInstanceOf[IngestorInitializationException]) shouldBe Some(ct,
+          i.error shouldBe a[ActorInitializationException]
+          i.error.asInstanceOf[ActorInitializationException].getActor.path shouldBe ct.path
+          ActorInitializationException
+            .unapply(i.error.asInstanceOf[ActorInitializationException]) shouldBe Some(ct,
             i.error.getMessage, i.error.getCause)
       }
     }
 
     it("handle errors by restarting") {
       system.actorOf(Props(classOf[TestIngestor], true, false)) ! "error"
+    }
+
+    it("calls the default init method") {
+      val act = system.actorOf(Props[TestIngestorDefault])
+      act ! "hello"
+
+      expectMsg("hi!")
+
+      act ! "timeout"
+      expectMsg(1.second) //testing that override with a val won't take effect until after the constructor ends
     }
 
     it("handle the base ingestion protocol") {
@@ -64,17 +75,30 @@ class IngestorSpec extends TestKit(ActorSystem("test")) with Matchers with FunSp
   }
 }
 
+class TestIngestorDefault extends Ingestor {
+  /**
+    * This will _not_ override; instead it will use the default value of 1.second. We'll test it.
+    */
+  override val initTimeout = 2.millisecond
+
+  val to = context.receiveTimeout
+
+  ingest {
+    case "hello" => sender ! "hi!"
+    case "timeout" => sender ! to
+  }
+}
+
 class TestIngestor(completeInit: Boolean, delayInit: Boolean) extends Ingestor {
 
   implicit val ec = context.dispatcher
 
-  val err = IngestorInitializationException(self, "ERROR")
+  val err = ActorInitializationException(self, "ERROR")
 
-  override def initIngestor: Future[HydraMessage] = {
-
+  override def init: Future[HydraMessage] = {
     Future {
       if (delayInit) Thread.sleep((initTimeout * 2).toMillis)
-      if (!completeInit) IngestorInitializationError(err) else IngestorInitialized
+      if (!completeInit) InitializationError(err) else Initialized
     }
   }
 
