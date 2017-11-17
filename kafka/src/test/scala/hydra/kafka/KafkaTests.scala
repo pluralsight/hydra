aeddf3e33d855a10aee680d4fd3bd7474295ba91
Alex Silva <alex-silva@pluralsight.com>
2017-11-17 13:35:37 -0500

    fixing kafka config

--- kafka/src/test/scala/hydra/kafka/KafkaTests.scala
+++ kafka/src/test/scala/hydra/kafka/KafkaTests.scala
@@ -9,8 +9,9 @@ import org.scalatest.{BeforeAndAfterAll, Suites}
 class KafkaTests extends Suites(new ZookeeperHealthCheckSpec, new KafkaHealthCheckSpec, new KafkaMetricsSpec,
   new KafkaConsumerProxySpec, new KafkaProducerProxySpec, new KafkaTransportSpec, new KafkaUtilsSpec)
   with BeforeAndAfterAll {
-  implicit val config = EmbeddedKafkaConfig(kafkaPort = 8092, zooKeeperPort = 3181)
-
+  implicit val config = EmbeddedKafkaConfig(kafkaPort = 8092, zooKeeperPort = 3181,
+    customBrokerProperties = Map("auto.create.topics.enable" -> "false"))
+  
   override def beforeAll(): Unit = EmbeddedKafka.start()
 
   override def afterAll() = EmbeddedKafka.stop()
