e252f748a3fa2609ad6805bb78ac5880c412fbff
Alex Silva <alex-silva@pluralsight.com>
2017-11-28 17:02:39 -0500

    fixing unit tests

--- ingest/src/test/scala/hydra/ingest/test/TestRecord.scala
+++
@@ -1,7 +0,0 @@
-package hydra.ingest.test
-
-import hydra.core.transport.HydraRecord
-
-case class TestRecord(destination: String,
-                      key: Option[String],
-                      payload: String) extends HydraRecord[String, String]
