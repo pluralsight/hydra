e9f7262f57dbbe574ed8411ac2a7f6af9cb7794e
Alex Silva <alex-silva@pluralsight.com>
2017-12-01 08:22:12 -0500

    finishing unit tests

--- jdbc/src/test/scala/hydra/jdbc/JdbcTransportSpec.scala
+++ jdbc/src/test/scala/hydra/jdbc/JdbcTransportSpec.scala
@@ -1,17 +1,34 @@
 package hydra.jdbc
 
+import java.sql.SQLException
+
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import hydra.common.util.TryWith
-import hydra.core.transport.HydraRecord
+import hydra.core.transport.{HydraRecord, RecordMetadata, TransportCallback, TransportSupervisor}
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
 import org.scalatest.{FunSpecLike, Matchers}
+
+import scala.io.Source
 
 class JdbcTransportSpec extends TestKit(ActorSystem("hydra-test")) with Matchers with FunSpecLike
   with ImplicitSender {
 
   val probe = TestProbe()
 
+  val schema = new Schema.Parser().parse(Source.fromResource("schema.avsc").mkString)
+
   val jdbcTransport = TestActorRef[JdbcTransport](Props[JdbcTransport], "jdbc_transport")
+
+  val ack: TransportCallback = (d: Long, md: Option[RecordMetadata], err: Option[Throwable]) =>
+    probe.ref ! md.map(_ => "DONE").getOrElse(err.get)
+
+
+  val gr = new GenericData.Record(schema)
+  gr.put("id", 1)
+  gr.put("name", "alex")
+  gr.put("rank", 2)
 
   describe("The Jdbc transport") {
     it("loads db profiles on preStart") {
@@ -27,6 +44,33 @@ class JdbcTransportSpec extends TestKit(ActorSystem("hydra-test")) with Matchers
       val profiles = jdbcTransport.underlyingActor.dbProfiles
       JdbcTransport.getUrl(profiles("test-dsprofile")) shouldBe "jdbc:h2:mem:test_db"
       JdbcTransport.getUrl(profiles("test-jdbcprofile")) shouldBe "jdbc:h2:mem:test_jdb;DB_CLOSE_DELAY=-1"
+    }
+
+    it("reports error if profile can't be found") {
+      val record = JdbcRecord("dest", Some(Seq.empty), gr, "dbProfile")
+      jdbcTransport ! TransportSupervisor.Deliver(record, 1, ack)
+      probe.expectMsgType[NoSuchElementException]
+    }
+
+    it("transports") {
+      val record = JdbcRecord("test_transport", Some(Seq.empty), gr, "test-dsprofile")
+      jdbcTransport ! TransportSupervisor.Deliver(record, 1, ack)
+      probe.expectMsg("DONE")
+      //check the db too
+      TryWith(jdbcTransport.underlyingActor.dbProfiles("test-dsprofile").ds.getConnection()) { c =>
+        val stmt = c.createStatement()
+        val rs = stmt.executeQuery("select \"id\",\"name\" from test_transport")
+        rs.next() shouldBe true
+      }
+    }
+
+    it("errors if underlying datasource is closed") {
+      val jt = TestActorRef[JdbcTransport](Props[JdbcTransport])
+      jt.underlyingActor.dbProfiles("test-dsprofile").ds.close()
+      val record = JdbcRecord("test_transport", Some(Seq.empty), gr, "test-dsprofile")
+      jt ! TransportSupervisor.Deliver(record, 1, ack)
+      probe.expectMsgType[SQLException]
+      system.stop(jt)
     }
   }
 
