782c8d2dd258cb4ed2695acca713f690474ea837
Alex Silva <alex-silva@pluralsight.com>
2018-02-05 14:52:46 -0700

    modifying class name for maps

--- core/src/main/scala/hydra/core/akka/KryoInit.scala
+++ core/src/main/scala/hydra/core/akka/KryoInit.scala
@@ -1,12 +1,12 @@
 package hydra.core.akka
 
 import com.esotericsoftware.kryo.Kryo
-import com.romix.scala.serialization.kryo.{EnumerationSerializer, ScalaImmutableMapSerializer, ScalaMutableMapSerializer}
+import com.romix.scala.serialization.kryo.{EnumerationSerializer, ScalaImmutableAbstractMapSerializer, ScalaMutableMapSerializer}
 
 class KryoInit {
   def customize(kryo: Kryo): Unit = {
     kryo.addDefaultSerializer(classOf[scala.Enumeration#Value], classOf[EnumerationSerializer])
     kryo.addDefaultSerializer(classOf[scala.collection.mutable.HashMap[_, _]], classOf[ScalaMutableMapSerializer])
-    kryo.addDefaultSerializer(classOf[scala.collection.Map[_, _]], classOf[ScalaImmutableMapSerializer])
+    kryo.addDefaultSerializer(classOf[scala.collection.Map[_, _]], classOf[ScalaImmutableAbstractMapSerializer])
   }
 }
