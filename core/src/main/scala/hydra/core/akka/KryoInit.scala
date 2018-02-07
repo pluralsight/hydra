4586308c8a0cc13dd3b3fc17fea7a1b5d92990fc
Alex Silva <alex-silva@pluralsight.com>
2018-02-07 09:07:55 -0700

    adding unit tests

--- core/src/main/scala/hydra/core/akka/KryoInit.scala
+++ core/src/main/scala/hydra/core/akka/KryoInit.scala
@@ -2,9 +2,11 @@ package hydra.core.akka
 
 import com.esotericsoftware.kryo.Kryo
 import com.romix.scala.serialization.kryo.{EnumerationSerializer, ScalaImmutableAbstractMapSerializer, ScalaMutableMapSerializer}
+import hydra.common.logging.LoggingAdapter
 
-class KryoInit {
+class KryoInit extends LoggingAdapter {
   def customize(kryo: Kryo): Unit = {
+    log.debug("Initializing Kryo...")
     kryo.addDefaultSerializer(classOf[scala.Enumeration#Value], classOf[EnumerationSerializer])
     kryo.addDefaultSerializer(classOf[scala.collection.mutable.HashMap[_, _]], classOf[ScalaMutableMapSerializer])
     kryo.addDefaultSerializer(classOf[scala.collection.Map[_, _]], classOf[ScalaImmutableAbstractMapSerializer])
--- core/src/test/scala/hydra/core/akka/KryoInitSpec.scala
+++ core/src/test/scala/hydra/core/akka/KryoInitSpec.scala
@@ -1,7 +1,10 @@
 package hydra.core.akka
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
 import com.esotericsoftware.kryo.Kryo
-import com.romix.scala.serialization.kryo.{EnumerationSerializer, ScalaImmutableMapSerializer, ScalaMutableMapSerializer}
+import com.esotericsoftware.kryo.io.{Input, Output}
+import com.romix.scala.serialization.kryo.{EnumerationSerializer, ScalaImmutableAbstractMapSerializer, ScalaMutableMapSerializer}
 import org.scalatest.{FlatSpecLike, Matchers}
 
 class KryoInitSpec extends Matchers with FlatSpecLike {
@@ -10,8 +13,38 @@ class KryoInitSpec extends Matchers with FlatSpecLike {
     val kryo = new Kryo()
     new KryoInit().customize(kryo)
     kryo.getDefaultSerializer(classOf[scala.Enumeration#Value]) shouldBe an[EnumerationSerializer]
-    kryo.getDefaultSerializer(classOf[scala.collection.Map[_, _]]) shouldBe a[ScalaImmutableMapSerializer]
+    kryo.getDefaultSerializer(classOf[collection.immutable.Map[_, _]]) shouldBe a[ScalaImmutableAbstractMapSerializer]
+    kryo.getDefaultSerializer(classOf[scala.collection.Map[_, _]]) shouldBe a[ScalaImmutableAbstractMapSerializer]
     kryo.getDefaultSerializer(classOf[scala.collection.mutable.HashMap[_, _]]) shouldBe a[ScalaMutableMapSerializer]
+  }
+
+  it should "serialize immutable maps" in {
+    val kryo = new Kryo()
+    new KryoInit().customize(kryo)
+    val map1 = Map("Rome" -> "Italy", "London" -> "England",
+      "Paris" -> "France", "New York" -> "USA", "Tokyo" -> "Japan",
+      "Peking" -> "China", "Brussels" -> "Belgium")
+    val map2 = map1 + ("Moscow" -> "Russia")
+    val map3 = map2 + ("Berlin" -> "Germany")
+    val map4 = map3 + ("Germany" -> "Berlin", "Russia" -> "Moscow")
+    roundTrip(map1, kryo)
+    roundTrip(map2, kryo)
+    roundTrip(map3, kryo)
+    roundTrip(map4, kryo)
+  }
+
+  def roundTrip[T](obj: T, kryo: Kryo): T = {
+    val outStream = new ByteArrayOutputStream()
+    val output = new Output(outStream, 4096)
+    kryo.writeClassAndObject(output, obj)
+    output.flush()
+
+    val input = new Input(new ByteArrayInputStream(outStream.toByteArray), 4096)
+    val obj1 = kryo.readClassAndObject(input)
+
+    assert(obj == obj1)
+
+    obj1.asInstanceOf[T]
   }
 
 }
