6357f8a1f10e6fd01ab208f269983950cd4a7f17
McKay Harris <mckayharris11@gmail.com>
2018-02-23 12:43:34 -0700

    moved notification params to hydra-notifications library

--- core/src/main/scala/hydra/core/ingest/NotificationParams.scala
+++
@@ -1,9 +0,0 @@
-package hydra.core.ingest
-
-// $COVERAGE-OFF
-object NotificationParams {
-  /**
-    * Used to publish to a Slack channel other than the default channel.
-    */
-  val SLACK_NOTIFICATION_CHANNEL_PARAM = "slack-notification-channel"
-}
