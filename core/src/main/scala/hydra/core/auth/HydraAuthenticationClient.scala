978868c939349d5b6e695a33285199b9d6723411
McKay Harris <mckayharris11@gmail.com>
2018-04-12 17:28:47 -0600

    adds initial draft of auth logic

---
+++ core/src/main/scala/hydra/core/auth/HydraAuthenticationClient.scala
@@ -0,0 +1,55 @@
+package hydra.core.auth
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers._
+import hydra.common.config.ConfigSupport
+import hydra.common.logging.LoggingAdapter
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+
+
+class HydraAuthenticationClient(implicit sys: ActorSystem, ec: ExecutionContext) extends HydraAuthenticator
+  with LoggingAdapter {
+  override def auth(credentials: Option[HttpCredentials]): Option[String] = {
+    import HydraAuthenticationClient._
+
+    val token = credentials match {
+      case Some(creds) => creds.token
+      case None => ""
+    }
+
+    val response = Http().singleRequest(buildRequest(token))
+
+    var result: Option[String] = None
+
+    response.onComplete {
+      case Success(_) => result = Some("joe")
+      case _ => result = None
+    }
+
+    result
+  }
+}
+
+
+object HydraAuthenticationClient extends ConfigSupport {
+  def buildRequest(token: String): HttpRequest = {
+    val serverUrl = applicationConfig.getString("auth.serverUrl")
+
+    val serverPort = applicationConfig.getInt("auth.serverPort")
+
+    val authUri = Uri(serverUrl).withPort(serverPort).withPath(Uri.Path("hydra-auth/me"))
+
+    val auth = Authorization(GenericHttpCredentials("Bearer", token))
+
+    HttpRequest(HttpMethods.POST, authUri, List(auth))
+  }
+
+  def parseResponse(response: HttpResponse): String = {
+    ""
+  }
+}
---
+++ core/src/test/scala/hydra/core/auth/HydraAuthenticationClientSpec.scala
@@ -0,0 +1,35 @@
+package hydra.core.auth
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.headers.GenericHttpCredentials
+import org.scalatest.{FlatSpec, MustMatchers}
+
+class HydraAuthenticationClientSpec extends FlatSpec
+  with MustMatchers {
+
+  implicit val sys = ActorSystem("hydra-auth-client")
+
+  "Authenticating with a valid token" must
+    "return the user's name" in {
+    val client = new HydraAuthenticationClient
+
+    val creds = GenericHttpCredentials("Bearer", "12345")
+
+    client.auth(Some(creds)) mustBe Some("john.doe")
+  }
+
+  "Authenticating without credentials" must
+    "return None" in {
+    fail()
+  }
+
+  "HydraAuthenticationClient" must
+    "build a request from a token" in {
+    fail()
+  }
+
+  it must
+    "parse the name from a JSON response" in {
+    fail()
+  }
+}
