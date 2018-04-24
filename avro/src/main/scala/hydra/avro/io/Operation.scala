3b878821a9a47c04d65fb177bf1c01e665b49dfd
Alex Silva <alex-silva@pluralsight.com>
2018-04-24 16:17:33 -0600

    Adds Delete operation to JdbcRecordWriter

--- avro/src/main/scala/hydra/avro/io/Operation.scala
+++ avro/src/main/scala/hydra/avro/io/Operation.scala
@@ -3,12 +3,8 @@ package hydra.avro.io
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 
-sealed trait Operation {
-  def shouldFlush: Boolean = false
-}
+sealed trait Operation
 
 case class Upsert(record: GenericRecord) extends Operation
 
-case class DeleteByKey(keys: Map[Schema.Field, AnyRef]) extends Operation {
-  override val shouldFlush: Boolean = true
-}
+case class DeleteByKey(keys: Map[Schema.Field, AnyRef]) extends Operation
