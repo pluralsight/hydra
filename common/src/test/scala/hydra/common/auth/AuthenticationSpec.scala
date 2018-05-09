01d0d386715fd1a917b339601ed36e38f08b39ab
Alex Silva <alex-silva@pluralsight.com>
2018-05-09 15:56:17 -0600

    Adding system as an implicit variable

--- common/src/test/scala/hydra/common/auth/AuthenticationSpec.scala
+++ common/src/test/scala/hydra/common/auth/AuthenticationSpec.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.headers.{BasicHttpCredentials, HttpCredentials, OAuth2BearerToken}
 import akka.testkit.TestKit
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FlatSpecLike, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.{ExecutionContext, Future}
@@ -12,23 +12,27 @@ import scala.concurrent.{ExecutionContext, Future}
 class AuthenticationSpec extends TestKit(ActorSystem("AuthenticationSpec"))
   with Matchers
   with FlatSpecLike
-  with ScalaFutures {
+  with ScalaFutures
+  with BeforeAndAfterAll {
 
-  "The Authenticator" should "delegate auth" in {
-    val authenticator = new TestAuthenticator()
+  override def afterAll = TestKit.shutdownActorSystem(system)
 
-    whenReady(authenticator.authenticate(Some(new BasicHttpCredentials("test", "")))) { r =>
-      r shouldBe Left(authenticator.challenge)
+    "The Authenticator" should "delegate auth" in {
+      val authenticator = new TestAuthenticator()
+
+      whenReady(authenticator.authenticate(Some(new BasicHttpCredentials("test", "")))) { r =>
+        r shouldBe Left(authenticator.challenge)
+      }
+
+      whenReady(authenticator.authenticate(Some(new BasicHttpCredentials("nice-user", "")))) { r =>
+        r shouldBe Right("nice-user")
+      }
+
+      whenReady(authenticator.authenticate(None)) { r =>
+        r shouldBe Left(authenticator.challenge)
+      }
     }
 
-    whenReady(authenticator.authenticate(Some(new BasicHttpCredentials("nice-user", "")))) { r =>
-      r shouldBe Right("nice-user")
-    }
-
-    whenReady(authenticator.authenticate(None)) { r =>
-      r shouldBe Left(authenticator.challenge)
-    }
-  }
   "The NoSecurityAuthenticator" should "always return true" in {
     whenReady(new NoSecurityAuthenticator().auth(Some(new BasicHttpCredentials("test", "test")))) { f =>
       f shouldBe "Anonymous"
@@ -40,7 +44,7 @@ class AuthenticationSpec extends TestKit(ActorSystem("AuthenticationSpec"))
 
   class TestAuthenticator extends HydraAuthenticator {
     override def auth(creds: Option[HttpCredentials])
-                     (implicit s:ActorSystem, ec: ExecutionContext): Future[String] = {
+                     (implicit s: ActorSystem, ec: ExecutionContext): Future[String] = {
       creds match {
         case Some(c) =>
           val c1 = c.asInstanceOf[BasicHttpCredentials]
@@ -49,4 +53,5 @@ class AuthenticationSpec extends TestKit(ActorSystem("AuthenticationSpec"))
       }
     }
   }
+
 }
