82c307f457176be6d2ad6ffbe137f447ca61f1d0
Alex Silva <alex-silva@pluralsight.com>
2018-06-18 11:53:30 -0600

    adding systemd notification listener

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,6 +47,8 @@ object Dependencies {
 
     val embeddedKafka = "net.manub" %% "scalatest-embedded-kafka" % "0.14.0"
 
+    val sdNotify = "info.faljse" % "SDNotify" % "1.1"
+
     val kafka = Seq(
       "org.apache.kafka" %% "kafka" % kafkaVersion,
       "org.apache.kafka" % "kafka-clients" % kafkaVersion,
@@ -144,7 +146,7 @@ object Dependencies {
   val avroDeps = baseDeps ++ confluent ++ jackson ++ Seq(guavacache)
 
   val coreDeps = akka ++ baseDeps ++
-    Seq(guavacache, reflections, serviceContainer, akkaKryo) ++ confluent ++ constructR
+    Seq(guavacache, reflections, serviceContainer, akkaKryo, sdNotify) ++ confluent ++ constructR
 
   val ingestDeps = coreDeps
 
