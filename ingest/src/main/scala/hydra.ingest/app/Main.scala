6007c8522f5fd643e0697f1fa19aaa2fe0990243
Alex Silva <alex-silva@pluralsight.com>
2018-06-22 09:57:27 -0600

    Adds Prometheus reporter

--- ingest/src/main/scala/hydra.ingest/app/Main.scala
+++ ingest/src/main/scala/hydra.ingest/app/Main.scala
@@ -1,9 +1,17 @@
 package hydra.ingest.app
 
 import hydra.core.bootstrap.BootstrappingSupport
+import kamon.Kamon
+import kamon.prometheus.PrometheusReporter
+import configs.syntax._
 
 // $COVERAGE-OFF$Disabling highlighting by default until a workaround for https://issues.scala-lang.org/browse/SI-8596 is found
 object Main extends App with BootstrappingSupport {
+  val enablePrometheus = applicationConfig.get[Boolean]("monitoring.prometheus.enable").valueOrElse(true)
+  if (enablePrometheus) {
+    Kamon.addReporter(new PrometheusReporter())
+  }
+
   containerService.start()
 }
 
