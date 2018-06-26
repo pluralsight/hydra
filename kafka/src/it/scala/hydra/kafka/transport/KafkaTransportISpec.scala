e2815403646d045da3984bf69a57b03680b725a4
ivanwakeup <ivan9miller@gmail.com>
2018-06-26 16:53:22 -0600

    WIP kafka transport I spec to test metrics are recorded

--- kafka/src/it/scala/hydra/kafka/transport/KafkaTransportISpec.scala
+++ kafka/src/it/scala/hydra/kafka/transport/KafkaTransportISpec.scala
@@ -4,20 +4,13 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.typesafe.config.{Config, ConfigFactory}
 import hydra.common.config.ConfigSupport
-import hydra.core.transport.Transport.Deliver
-import hydra.core.transport.{RecordMetadata, TransportCallback}
 import hydra.kafka.producer.{DeleteTombstoneRecord, JsonRecord, KafkaRecordMetadata, StringRecord}
-import hydra.kafka.transport.KafkaProducerProxy.ProducerInitializationError
-import hydra.kafka.transport.KafkaTransport.RecordProduceError
 import kamon.MetricReporter
 import kamon.metric.PeriodSnapshot
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
-import org.apache.kafka.common.KafkaException
-import org.apache.kafka.common.errors.SerializationException
 import org.scalatest.concurrent.Eventually
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
 
-import scala.concurrent.duration._
 
 class KafkaTransportISpec extends TestKit(ActorSystem("hydra"))
   with Matchers
@@ -28,9 +21,6 @@ class KafkaTransportISpec extends TestKit(ActorSystem("hydra"))
   with ConfigSupport {
 
   lazy val transport = system.actorOf(KafkaTransport.props(rootConfig), "kafka")
-
-  implicit val config = EmbeddedKafkaConfig(kafkaPort = 8092, zooKeeperPort = 3181,
-    customBrokerProperties = Map("auto.create.topics.enable" -> "false"))
 
   val reporter = new MetricReporter {
 
@@ -47,16 +37,32 @@ class KafkaTransportISpec extends TestKit(ActorSystem("hydra"))
     override def reconfigure(config: Config): Unit = {}
   }
 
+  override def beforeAll() = {
+    super.beforeAll()
+    EmbeddedKafka.start()
+    EmbeddedKafka.createCustomTopic("transport_test")
+  }
+
+  override def afterAll() = {
+    super.afterAll()
+    system.stop(transport)
+    EmbeddedKafka.stop()
+    TestKit.shutdownActorSystem(system, verifySystemShutdown = true)
+  }
+
+
   val metadata = KafkaRecordMetadata(1, 1L, "test.topic", 2, 3)
 
   describe("When using the KafkaTransport") {
 
     it("should increment message rate counters") {
-      transport ! KafkaRecordMetadata
+      transport ! metadata
       eventually {
-        reporter.snapshot.metrics.counters
+        val snapshot = reporter.snapshot
+        snapshot.metrics.counters
           .filter(_.name == KafkaTransport.counterMetricName)
-
+          .head
+          .value shouldBe 1
       }
     }
   }
