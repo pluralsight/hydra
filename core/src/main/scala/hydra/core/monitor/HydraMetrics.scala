4cef5b1a374c77e3feef3cf2ee81aecd31d76ceb
McKay Harris <mckayharris11@gmail.com>
2018-07-02 10:03:17 -0600

    Defines "Tags" type to give more meaningful method signatures

--- core/src/main/scala/hydra/core/monitor/HydraMetrics.scala
+++ core/src/main/scala/hydra/core/monitor/HydraMetrics.scala
@@ -6,32 +6,34 @@ import kamon.metric.{Counter, Gauge, Histogram}
 import scala.collection.mutable
 
 trait HydraMetrics {
+  type Tags = Seq[(String, String)]
+
   private[core] lazy val counters = new mutable.HashMap[String, Counter]()
   private[core] lazy val gauges = new mutable.HashMap[String, Gauge]()
   private[core] lazy val histograms = new mutable.HashMap[String, Histogram]()
 
-  def incrementCounter(lookupKey: String, metricName: String, tags: => Seq[(String, String)]): Unit = {
+  def incrementCounter(lookupKey: String, metricName: String, tags: => Tags): Unit = {
     counters
       .getOrElseUpdate(lookupKey,
         Kamon.counter(metricName).refine(tags: _*))
       .increment()
   }
 
-  def incrementGauge(lookupKey: String, metricName: String, tags: => Seq[(String, String)]): Unit = {
+  def incrementGauge(lookupKey: String, metricName: String, tags: => Tags): Unit = {
     gauges
       .getOrElseUpdate(lookupKey,
         Kamon.gauge(metricName).refine(tags: _*))
       .increment()
   }
 
-  def decrementGauge(lookupKey: String, metricName: String, tags: => Seq[(String, String)]): Unit = {
+  def decrementGauge(lookupKey: String, metricName: String, tags: => Tags): Unit = {
     gauges
       .getOrElseUpdate(lookupKey,
         Kamon.gauge(metricName).refine(tags: _*))
       .decrement()
   }
 
-  def recordToHistogram(lookupKey: String, metricName: String, value: Long, tags: => Seq[(String, String)]): Unit = {
+  def recordToHistogram(lookupKey: String, metricName: String, value: Long, tags: => Tags): Unit = {
     histograms
       .getOrElseUpdate(lookupKey,
         Kamon.histogram(metricName).refine(tags: _*))
