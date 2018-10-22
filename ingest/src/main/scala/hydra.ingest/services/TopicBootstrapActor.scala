62c51bfe337c272b956076695811b53a56c44607
McKay Harris <mckayharris11@gmail.com>
2018-10-22 14:34:07 -0600

    Refactor TopicBootstrapActor to fetch schema in prestart and become the correct behavior in the callback

--- ingest/src/main/scala/hydra.ingest/services/TopicBootstrapActor.scala
+++ ingest/src/main/scala/hydra.ingest/services/TopicBootstrapActor.scala
@@ -1,18 +1,20 @@
 package hydra.ingest.services
 
+import akka.actor.Status.Failure
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.http.scaladsl.model.StatusCodes
+import akka.pattern.ask
+import akka.util.Timeout
 import com.typesafe.config.Config
-import hydra.core.http.ImperativeRequestContext
+import configs.syntax._
+import hydra.avro.resource.SchemaResource
+import hydra.core.akka.SchemaRegistryActor.{FetchSchemaResponse, RegisterSchemaRequest}
 import hydra.core.ingest.{HydraRequest, RequestParams}
 import hydra.core.marshallers.{HydraJsonSupport, TopicMetadataRequest}
 import hydra.core.protocol.InitiateHttpRequest
 import hydra.ingest.services.TopicBootstrapActor._
-import spray.json._
-import akka.pattern.ask
 
 import scala.concurrent.duration._
-import configs.syntax._
 
 class TopicBootstrapActor(
                          config: Config,
@@ -20,23 +22,39 @@ class TopicBootstrapActor(
                          ingestionHandlerGateway: ActorRef,
                          ) extends Actor with HydraJsonSupport with ActorLogging {
 
-  override def receive: Receive = {
-    case InitiateTopicBootstrap(topicMetadataRequest) => {
-      initiateBootstrap(topicMetadataRequest)
+  implicit val ec = context.dispatcher
+
+  implicit val timeout = Timeout(10.seconds)
+
+  override def receive: Receive = initializing
+
+  override def preStart(): Unit = {
+    (schemaRegistryActor ? RegisterSchemaRequest()) foreach {
+      case FetchSchemaResponse(schema) => context.become(active(schema))
+      case Failure(ex) => context.become(failed)
     }
   }
 
-  override def preStart(): Unit = {
-    schemaRegistryActor ?
+  def initializing: Receive = {
+    case _ => sender ! ActorInitializing
+  }
+
+  def active(schema: SchemaResource): Receive = {
+    case InitiateTopicBootstrap(topicMetadataRequest) =>
+  }
+
+  def failed(ex: Throwable): Receive = {
+    case _ =>
+      BootstrapFailure(s"TopicBootstrapActor is in an errored state due to ${ex.getCause.getMessage}")
   }
 
   private[ingest] def initiateBootstrap(topicMetadataRequest: TopicMetadataRequest): Unit = {
-    val enrichedRequest = enrichRequest(hydraRequest)
-    val result: BootstrapResult = validateTopicName(mdRequest)
+    val enrichedRequest = enrichRequest(topicMetadataRequest)
+    val result: BootstrapResult = validateTopicName(topicMetadataRequest)
     result match {
-      case BootstrapStepSuccess =>
+      case BootstrapSuccess =>
         ingestionHandlerGateway ! InitiateHttpRequest(enrichedRequest, 100.millis, ctx)
-      case BootstrapStepFailure(reasons) =>
+      case BootstrapFailure(reasons) =>
         ctx.complete(StatusCodes.BadRequest,
           s"Topic name is invalid for the following reasons: $reasons")
     }
@@ -45,14 +63,14 @@ class TopicBootstrapActor(
   private[ingest] def validateTopicName(topicMetadataRequest: TopicMetadataRequest): BootstrapResult = {
     val isValidOrErrorReport = TopicNameValidator.validate(topicMetadataRequest.streamName)
     isValidOrErrorReport match {
-      case Valid => BootstrapStepSuccess
+      case Valid => BootstrapSuccess
       case InvalidReport(reasons) =>
         val invalidDisplayString = reasons
           .map(_.reason)
           .map("\t" + _)
           .mkString("\n")
-        BootstrapStepFailure(invalidDisplayString)
-      case _ => BootstrapStepFailure("Couldn't find match on validateTopicName")
+        BootstrapFailure(invalidDisplayString)
+      case _ => BootstrapFailure("Couldn't find match on validateTopicName")
     }
   }
 
@@ -79,8 +97,11 @@ object TopicBootstrapActor {
 
   case class ForwardBootstrapPayload(request: HydraRequest) extends TopicBootstrapMessage
 
-
   sealed trait BootstrapResult
-  case object BootstrapStepSuccess extends BootstrapResult
-  case class BootstrapStepFailure(reasons: String) extends BootstrapResult
+
+  case object BootstrapSuccess extends BootstrapResult
+
+  case class BootstrapFailure(reasons: String) extends BootstrapResult
+
+  case object ActorInitializing extends BootstrapResult
 }
