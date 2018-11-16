8c33e13ff27a789ccf76fa8a1661ad08483e9db5
McKay Harris <mckayharris11@gmail.com>
2018-11-16 10:57:37 -0700

    Adds first ispec stub for token repository

---
+++ common/src/it/scala/hydra/common/auth/TokenRepositoryISpec.scala
@@ -0,0 +1,11 @@
+package hydra.common.auth
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TokenRepositoryISpec extends FlatSpec
+  with Matchers {
+
+  "A TokenRepository" should "retrieve token info from a storage backend" in {
+
+  }
+}
