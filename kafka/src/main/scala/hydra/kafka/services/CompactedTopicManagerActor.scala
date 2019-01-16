ab222c5009273c08938ae555fb257b76dec6a4d3
ivanwakeup <ivan9miller@gmail.com>
2019-01-16 12:26:47 -0700

    wip

--- kafka/src/main/scala/hydra/kafka/services/CompactedTopicManagerActor.scala
+++ kafka/src/main/scala/hydra/kafka/services/CompactedTopicManagerActor.scala
@@ -2,23 +2,17 @@ package hydra.kafka.services
 
 import java.util.concurrent.TimeUnit
 
-import akka.NotUsed
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.kafka.scaladsl.Consumer.Control
-import akka.pattern.pipe
-import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.Config
 import hydra.common.config.ConfigSupport
-import hydra.kafka.model.TopicMetadata
-import hydra.kafka.services.CompactedTopicManagerActor.{CompactedTopicManagerResult, _}
-import hydra.kafka.services.TopicBootstrapActor.{BootstrapFailure, BootstrapSuccess}
+import hydra.kafka.services.CompactedTopicManagerActor._
 import hydra.kafka.util.KafkaUtils
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
 import org.apache.kafka.common.requests.CreateTopicsRequest.TopicDetails
 
-import scala.concurrent.duration._
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import akka.pattern.pipe
 
 class CompactedTopicManagerActor(consumerConfig: Config,
                             bootstrapServers: String,
@@ -28,36 +22,25 @@ class CompactedTopicManagerActor(consumerConfig: Config,
   with ConfigSupport
   with ActorLogging {
 
-  import MetadataConsumerActor._
-
-  //private val metadataMap = new collection.mutable.HashMap[String, TopicMetadata]()
-
-  private implicit val ec = context.dispatcher
-
-  private implicit val materializer: Materializer = ActorMaterializer()
-
-  private val stream = MetadataConsumerActor.createStream(consumerConfig, bootstrapServers,
-    schemaRegistryClient, metadataTopicName, self)
+  //maintains map of actorRefs, we can use the actorRef to query the actor and get its status?
+  private val compactedStreamsMap = new collection.mutable.HashMap[String, String]()
+  private final val COMPACTED_PREFIX = "_compacted."
 
   override def receive: Receive = {
-    case CreateCompactedStream(topicName) => //create the compacted stream if it doesn't exist already
-    case CreateCompactedTopic(topicName, topicDetails) => //create the compacted topic if it doesn't exist already
-  }
 
-  override def preStart(): Unit = {
-    context.become(streaming(stream.run()))
-  }
+    case CreateCompactedTopic(topicName, topicDetails) => {
+      createCompactedTopic(topicName, topicDetails).map { _ =>
+        self ! CreateCompactedStream(topicName)
+      }.recover {
+        case e: Exception => throw e
+      }
+    }
+
+    case CreateCompactedStream(topicName) => {
+      pipe(createCompactedStream(topicName)) to sender
+    }
 
 
-  def streaming(stream: (Control, NotUsed)): Receive = {
-    case GetMetadata =>
-      sender ! GetMetadataResponse(metadataMap.toMap)
-
-    case t: TopicMetadata =>
-      metadataMap.put(t.id.toString, t)
-
-    case StopStream =>
-      pipe(stream._1.shutdown().map(_ => StreamStopped)) to sender
   }
 
   private[kafka] def createCompactedTopic(topicName: String, topicDetails: TopicDetails): Future[Unit] = {
@@ -90,12 +73,17 @@ class CompactedTopicManagerActor(consumerConfig: Config,
     }
   }
 
+  private[kafka] def createCompactedStream(topicName: String): Future[Unit] = {
+    //do we want to return a future unit? how do we signal to the client that compacted was successful?
+    val compactedActor = context.actorOf()
+  }
+
 }
 
 object CompactedTopicManagerActor {
 
   case class CreateCompactedStream(topicName: String)
-  case class CreateCompactedTopic(topicName: String)
+  case class CreateCompactedTopic(topicName: String, topicDetails: TopicDetails)
 
   sealed trait CompactedTopicManagerResult
 
@@ -105,54 +93,6 @@ object CompactedTopicManagerActor {
             metadataTopicName: String) = {
     Props(classOf[CompactedTopicManagerActor], consumerConfig, bootstrapServers, schemaRegistryClient, metadataTopicName)
   }
-
-  /*
-  private type Stream = RunnableGraph[(Control, NotUsed)]
-
-  case object GetMetadata
-
-  case class GetMetadataResponse(metadata: Map[String, TopicMetadata])
-
-  case object StopStream
-
-  case object StreamStopped
-
-  private[services] def createStream[K, V](config: Config,
-                                           bootstrapSevers: String,
-                                           schemaRegistryClient: SchemaRegistryClient,
-                                           metadataTopicName: String,
-                                           destination: ActorRef)
-                                          (implicit ec: ExecutionContext, mat: Materializer): Stream = {
-
-    val formatter = ISODateTimeFormat.basicDateTimeNoMillis()
-
-    val settings = ConsumerSettings(config, new StringDeserializer,
-      new KafkaAvroDeserializer(schemaRegistryClient))
-      .withBootstrapServers(bootstrapSevers)
-      .withGroupId("metadata-consumer-actor")
-      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-
-    Consumer.plainSource(settings, Subscriptions.topics(metadataTopicName))
-      .map { msg =>
-        val record = msg.value.asInstanceOf[GenericRecord]
-        TopicMetadata(
-          record.get("subject").toString,
-          record.get("schemaId").toString.toInt,
-          record.get("streamType").toString,
-          record.get("derived").toString.toBoolean,
-          record.get("dataClassification").toString,
-          record.get("contact").toString,
-          Option(record.get("additionalDocumentation")).map(_.toString),
-          Option(record.get("notes")).map(_.toString),
-          UUID.fromString(record.get("id").toString),
-          formatter.parseDateTime(record.get("createdDate").toString),
-        )
-      }.toMat(Sink.actorRef(destination, StreamStopped))(Keep.both)
-  }
-
-
-
-  */
 
 }
 
--- kafka/src/main/scala/hydra/kafka/services/CompactedTopicStreamActor.scala
+++ kafka/src/main/scala/hydra/kafka/services/CompactedTopicStreamActor.scala
@@ -1,41 +1,29 @@
 package hydra.kafka.services
 
-import java.util.UUID
-
 import akka.NotUsed
 import akka.actor.{Actor, Props}
-import akka.kafka.{ConsumerSettings, ProducerMessage, ProducerSettings, Subscriptions}
-import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.kafka.scaladsl.Consumer.Control
-import akka.pattern.pipe
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.kafka.{ConsumerSettings, ProducerMessage, ProducerSettings, Subscriptions}
 import akka.stream.scaladsl.{Keep, RunnableGraph}
 import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.Config
 import hydra.common.config.ConfigSupport
-import hydra.kafka.model.TopicMetadata
-import hydra.kafka.services.CompactedTopicManagerActor.CreateCompactedStream
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
-import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.joda.time.format.ISODateTimeFormat
 
 import scala.concurrent.ExecutionContext
 
-class CompactedTopicStreamActor(topicName: String) extends Actor
+class CompactedTopicStreamActor(fromTopic: String, toTopic: String, bootstrapServers: String, config: Config) extends Actor
   with ConfigSupport {
 
-  import MetadataConsumerActor._
-
-  //private val metadataMap = new collection.mutable.HashMap[String, TopicMetadata]()
 
   private implicit val ec = context.dispatcher
 
   private implicit val materializer: Materializer = ActorMaterializer()
 
-  private val stream = MetadataConsumerActor.createStream(consumerConfig, bootstrapServers,
-    schemaRegistryClient, metadataTopicName, self)
+  private val stream = CompactedTopicStreamActor.createStream(config, bootstrapServers, fromTopic, toTopic)
 
   override def receive: Receive = {
     Actor.emptyBehavior
@@ -46,14 +34,6 @@ class CompactedTopicStreamActor(topicName: String) extends Actor
   }
 
   def streaming(stream: (Control, NotUsed)): Receive = {
-//    case GetMetadata =>
-//      sender ! GetMetadataResponse(metadataMap.toMap)
-//
-//    case t: TopicMetadata =>
-//      metadataMap.put(t.id.toString, t)
-//
-//    case StopStream =>
-//      pipe(stream._1.shutdown().map(_ => StreamStopped)) to sender
     Actor.emptyBehavior
   }
 
@@ -65,50 +45,33 @@ object CompactedTopicStreamActor {
 
   case class CreateCompactedStream(topicName: String)
 
-  def props(topicName: String) = {
-    Props(classOf[CompactedTopicStreamActor], topicName)
+  def props(fromTopic: String, toTopic: String, bootstrapServers: String, config: Config) = {
+    Props(classOf[CompactedTopicStreamActor], fromTopic, toTopic, bootstrapServers, config)
   }
 
 
   private[services] def createStream[K, V](config: Config,
                                            bootstrapSevers: String,
-                                           topicName: String,
-                                           destinationTopic: String)
+                                           fromTopic: String,
+                                           toTopic: String)
                                           (implicit ec: ExecutionContext, mat: Materializer): Stream = {
 
     val formatter = ISODateTimeFormat.basicDateTimeNoMillis()
 
     val consumerSettings = ConsumerSettings(config, None, None)
+      .withBootstrapServers(bootstrapSevers)
+      .withGroupId(toTopic)
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
     val producerSettings = ProducerSettings(config, None, None)
 
-//    val settings = ConsumerSettings(config, keyDesnew StringDeserializer,
-//      .withBootstrapServers(bootstrapSevers)
-//      .withGroupId("metadata-consumer-actor")
-//      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-    Consumer.plainSource(consumerSettings, Subscriptions.topics(topicName))
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(fromTopic))
       .map { msg =>
-         ProducerMessage.single(new ProducerRecord(destinationTopic, msg.key(), msg.value())
+         ProducerMessage.single(new ProducerRecord(toTopic, msg.key(), msg.value())
          )
       }.toMat(Producer.commitableSink(producerSettings))(Keep.both)
   }
 
-  /*
-  private type Stream = RunnableGraph[(Control, NotUsed)]
-
-  case object GetMetadata
-
-  case class GetMetadataResponse(metadata: Map[String, TopicMetadata])
-
-  case object StopStream
-
-  case object StreamStopped
-
-
-
-
-
-  */
 
 }
 
