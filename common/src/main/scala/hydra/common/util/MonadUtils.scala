02164c8ec6f4dc46664f95436bca7c730fadf0f6
ivanwakeup <ivan9miller@gmail.com>
2019-04-16 14:12:18 -0600

    fixt the 'superfluous extra space'

--- common/src/main/scala/hydra/common/util/MonadUtils.scala
+++ common/src/main/scala/hydra/common/util/MonadUtils.scala
@@ -1,9 +1,5 @@
 package hydra.common.util
 
-import scala.util.{Failure, Try}
-
 object MonadUtils {
-
   def booleanToOption[A](check:Boolean)(body:()=>Option[A]):Option[A] = if (check) body.apply else None
-
 }
