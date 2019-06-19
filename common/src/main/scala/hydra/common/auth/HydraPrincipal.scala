5fc6e5ca407104b7debb1181906a1763fd212cc6
McKay Harris <mckayharris11@gmail.com>
2019-06-19 15:25:33 -0600

    Remove authenticator piece of HydraSettings class

--- common/src/main/scala/hydra/common/auth/HydraPrincipal.scala
+++
@@ -1,7 +0,0 @@
-package hydra.common.auth
-
-import java.security.Principal
-
-case class HydraPrincipal(name: String, roles: Set[String], token: Option[String] = None) extends Principal {
-  override def getName: String = name
-}
