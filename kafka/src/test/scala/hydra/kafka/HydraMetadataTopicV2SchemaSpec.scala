13678cce4a5820985968d8403d49ab1335a3e47a
Jeff Lewis <lewisjkl@me.com>
2020-02-14 13:12:51 -0700

    update HydraMetadataTopicV2SchemaSpec

--- kafka/src/test/scala/hydra/kafka/HydraMetadataTopicV2SchemaSpec.scala
+++ kafka/src/test/scala/hydra/kafka/HydraMetadataTopicV2SchemaSpec.scala
@@ -1,23 +1,17 @@
 package hydra.kafka
 
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers
-import org.apache.avro.Schema.Parser
-import scala.io.Source
+import hydra.kafka.config.MetadataSchemaConfig
 import org.apache.avro.Schema
+import org.scalatest.{FlatSpec, Matchers}
 
 class HydraMetadataTopicV2SchemaSpec extends FlatSpec with Matchers {
 
     it should "be able to parse the HydraMetadataTopicValueV2.avsc into the Schema class" in {
-        val jsonSchemaString: String = Source.fromResource("HydraMetadataTopicValueV2.avsc").mkString
-        val schema = new Parser().parse(jsonSchemaString)
-        schema shouldBe a[Schema]
+        MetadataSchemaConfig.keySchema shouldBe a[Schema]
     }
 
     it should "be able to parse the HydraMetadataTopicKeyV2.avsc into the Schema Class" in {
-        val jsonSchemaString: String = Source.fromResource("HydraMetadataTopicKeyV2.avsc").mkString
-        val schema = new Parser().parse(jsonSchemaString)
-        schema shouldBe a[Schema]
+        MetadataSchemaConfig.valueSchema shouldBe a[Schema]
     }
 
 }
