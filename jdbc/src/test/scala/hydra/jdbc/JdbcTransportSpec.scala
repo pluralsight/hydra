247cb78bc022e2bf40cbad22ac7171b4d6c62c8a
Bret Smith <bretrbs@icloud.com>
2020-02-20 13:46:38 -0700

    Putting retry strategy around DBProfile close

--- jdbc/src/test/scala/hydra/jdbc/JdbcTransportSpec.scala
+++ jdbc/src/test/scala/hydra/jdbc/JdbcTransportSpec.scala
@@ -4,17 +4,25 @@ import java.sql.SQLException
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import cats.effect.{IO, Timer}
+import cats.implicits._
+import hydra.common.logging.LoggingAdapter
 import hydra.common.util.TryWith
 import hydra.core.transport.Transport.Deliver
 import hydra.core.transport.{AckStrategy, HydraRecord, RecordMetadata, TransportCallback}
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
+import retry.RetryDetails._
+import retry.RetryPolicies._
+import retry.{RetryPolicy, _}
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 import scala.io.Source
 
 class JdbcTransportSpec extends TestKit(ActorSystem("jdbc-transport-spec")) with Matchers with FunSpecLike
-  with ImplicitSender with BeforeAndAfterAll {
+  with ImplicitSender with BeforeAndAfterAll with LoggingAdapter {
 
   val probe = TestProbe()
 
@@ -62,13 +70,32 @@ class JdbcTransportSpec extends TestKit(ActorSystem("jdbc-transport-spec")) with
       }
     }
 
+    val retryPolicy: RetryPolicy[IO] = limitRetries[IO](4) |+| exponentialBackoff[IO](500 millis)
+    implicit val sleep: Timer[IO] = IO.timer(ExecutionContext.global)
+
+    def logError(err: Throwable, details: RetryDetails): IO[Unit] = details match {
+      case WillDelayAndRetry(_: FiniteDuration, retriesSoFar: Int, _: FiniteDuration) =>
+        IO(log.debug(s"DBProfile not yet closed. So far we have retried $retriesSoFar times."))
+      case GivingUp(totalRetries: Int, duration: FiniteDuration) =>
+        IO(log.debug(s"DBProfile could not be closed. Giving up after $totalRetries retries and ${duration.toSeconds} seconds."))
+    }
+
     it("errors if underlying datasource is closed") {
       val jt = TestActorRef[JdbcTransport](Props[JdbcTransport])
-      jt.underlyingActor.dbProfiles("test-dsprofile").close()
-      val record = JdbcRecord("test_transport", Seq.empty, gr, "test-dsprofile", AckStrategy.NoAck)
-      jt ! Deliver(record, 1, ack)
-      probe.expectMsgType[SQLException]
-      system.stop(jt)
+      val profile = jt.underlyingActor.dbProfiles("test-dsprofile")
+      profile.close()
+
+      def profileIsClosed = if (profile.isClosed) IO.unit else IO.raiseError(new Exception)
+      val flakyRequestWithRetry: IO[Unit] = retryingOnAllErrors[Unit](policy = retryPolicy, onError = logError)(profileIsClosed)
+
+      (for {
+        _ <- flakyRequestWithRetry
+      } yield {
+        val record = JdbcRecord("test_transport", Seq.empty, gr, "test-dsprofile", AckStrategy.NoAck)
+        jt ! Deliver(record, 1, ack)
+        probe.expectMsgType[SQLException]
+        system.stop(jt)
+      }).unsafeRunSync()
     }
   }
 
