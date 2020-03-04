e520cbec424ce39777321d063916066dd242c1cc
Scala Steward <me@scala-steward.org>
2020-03-04 04:34:46 +0100

    Update scalamock to 4.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   val refinedVersion = "0.9.12"
   val reflectionsVersion = "0.9.11"
   val scalaCacheVersion = "0.28.0"
-  val scalaMockVersion = "4.1.0"
+  val scalaMockVersion = "4.4.0"
   val scalaTestVersion = "3.0.5"
   val scalazVersion = "7.2.9"
   val serviceContainerVersion = "2.0.7"
