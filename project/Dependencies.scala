064172e9d5168dc59ac7655222738a2cab0ce77a
Scala Steward <me@scala-steward.org>
2020-03-13 17:04:55 +0100

    Update akka-actor, akka-persistence, ... to 2.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val akkaHTTPVersion = "10.1.11"
   val akkaKafkaStreamVersion = "2.0.2"
   val akkaKryoVersion = "0.5.2"
-  val akkaVersion = "2.6.3"
+  val akkaVersion = "2.6.4"
   val avroVersion = "1.9.2"
   val catsEffectVersion = "2.1.2"
   val catsLoggerVersion = "1.0.1"
