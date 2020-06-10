c481be6ef047432834f9045783a82e1299e222fc
Jeff Lewis <lewisjkl@me.com>
2020-06-10 11:16:30 -0600

    add v2 ingest flow

---
+++ ingest/src/main/scala/hydra.ingest/services/IngestionFlowV2.scala
@@ -0,0 +1,96 @@
+package hydra.ingest.services
+
+import java.io.IOException
+
+import cats.MonadError
+import cats.implicits._
+import com.pluralsight.hydra.avro.{JsonConverter, JsonToAvroConversionException}
+import hydra.avro.registry.SchemaRegistry
+import hydra.avro.resource.SchemaResourceLoader.SchemaNotFoundException
+import hydra.avro.util.SchemaWrapper
+import hydra.core.transport.ValidationStrategy
+import hydra.kafka.algebras.KafkaClientAlgebra
+import hydra.kafka.model.TopicMetadataV2Request.Subject
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericRecord
+import scalacache._
+import scalacache.guava._
+import scalacache.memoization._
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Try}
+
+final class IngestionFlowV2[F[_]: MonadError[*[_], Throwable]: Mode](
+                                                                    schemaRegistry: SchemaRegistry[F],
+                                                                    kafkaClient: KafkaClientAlgebra[F],
+                                                                    schemaRegistryBaseUrl: String) {
+
+  import IngestionFlowV2._
+
+  implicit val guavaCache: Cache[SchemaWrapper] = GuavaCache[SchemaWrapper]
+
+  private def getSchema(subject: String): F[Schema] = {
+    schemaRegistry.getLatestSchemaBySubject(subject)
+      .flatMap { maybeSchema =>
+        val schemaNotFound = SchemaNotFoundException(subject)
+        MonadError[F, Throwable].fromOption(maybeSchema, SchemaNotFoundAugmentedException(schemaNotFound, subject))
+      }
+  }
+
+  private def getSchemaWrapper(subject: Subject, isKey: Boolean): F[SchemaWrapper] = memoizeF[F, SchemaWrapper](Some(2.minutes)) {
+    val suffix = if (isKey) "-key" else "-value"
+    getSchema(subject.value + suffix).map { sch =>
+      SchemaWrapper.from(sch)
+    }
+  }
+
+  private def recover[A](subject: Subject, isKey: Boolean): PartialFunction[Throwable, Try[A]] = {
+    val suffix = if (isKey) "-key" else "-value"
+    val location = s"$schemaRegistryBaseUrl/subjects/${subject.value}$suffix/versions/latest/schema"
+    val pf: PartialFunction[Throwable, Try[A]] = {
+      case e: JsonToAvroConversionException =>
+        Failure(AvroConversionAugmentedException(s"${e.getClass.getName}: ${e.getMessage} [$location]"))
+      case e: IOException =>
+        Failure(AvroConversionAugmentedException(s"${e.getClass.getName}: ${e.getMessage} [$location]"))
+      case e => Failure(e)
+    }
+    pf
+  }
+
+  private def getSchemas(request: V2IngestRequest): F[(GenericRecord, Option[GenericRecord])] = {
+    val useStrictValidation = request.validationStrategy == ValidationStrategy.Strict
+    def getRecord(payload: String, schema: Schema): Try[GenericRecord] =
+      payload.toGenericRecord(schema, useStrictValidation)
+    for {
+      kSchema <- getSchemaWrapper(request.topic, isKey = true)
+      vSchema <- getSchemaWrapper(request.topic, isKey = false)
+      k <- MonadError[F, Throwable].fromTry(
+        getRecord(request.keyPayload, kSchema.schema).recoverWith(recover(request.topic, isKey = true)))
+      v <- MonadError[F, Throwable].fromTry(
+        request.valPayload.traverse(getRecord(_, vSchema.schema)).recoverWith(recover(request.topic, isKey = false)))
+    } yield (k, v)
+  }
+
+  def ingest(request: V2IngestRequest): F[Unit] = {
+    getSchemas(request).flatMap { case (key, value) =>
+      kafkaClient.publishMessage((key, value), request.topic.value).void
+    }
+  }
+}
+
+object IngestionFlowV2 {
+
+  private implicit class ConvertToGenericRecord(s: String) {
+    def toGenericRecord(schema: Schema, useStrictValidation: Boolean): Try[GenericRecord] = {
+      val converter: JsonConverter[GenericRecord] =
+        new JsonConverter[GenericRecord](schema, useStrictValidation)
+      Try(converter.convert(s))
+    }
+  }
+
+  final case class V2IngestRequest(topic: Subject, keyPayload: String, valPayload: Option[String], validationStrategy: ValidationStrategy)
+
+  final case class AvroConversionAugmentedException(message: String) extends RuntimeException(message)
+  final case class SchemaNotFoundAugmentedException(schemaNotFoundException: SchemaNotFoundException, topic: String)
+    extends RuntimeException(s"Schema '$topic' cannot be loaded. Cause: ${schemaNotFoundException.getClass.getName}: Schema not found for $topic")
+}
---
+++ ingest/src/test/scala/hydra/ingest/services/IngestionFlowV2Spec.scala
@@ -0,0 +1,57 @@
+package hydra.ingest.services
+
+import cats.effect.{Concurrent, ContextShift, IO}
+import cats.implicits._
+import hydra.avro.registry.SchemaRegistry
+import hydra.core.transport.ValidationStrategy
+import hydra.ingest.services.IngestionFlowV2.V2IngestRequest
+import hydra.kafka.algebras.KafkaClientAlgebra
+import hydra.kafka.model.TopicMetadataV2Request.Subject
+import org.apache.avro.{Schema, SchemaBuilder}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.ExecutionContext
+
+final class IngestionFlowV2Spec extends AnyFlatSpec with Matchers {
+
+  private implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  private implicit val concurrentEffect: Concurrent[IO] = IO.ioConcurrentEffect
+  private implicit val mode: scalacache.Mode[IO] = scalacache.CatsEffect.modes.async
+
+  private val testSubject: Subject = Subject.createValidated("exp.test.v0.Testing").get
+
+  private val testKey: String = "test"
+
+  private val testKeyPayload: String =
+    """{"id": "testing"}"""
+
+  private val testValPayload: String =
+    s"""{"testField": true}"""
+
+  private val testKeySchema: Schema = SchemaBuilder.record("TestRecord")
+    .fields().requiredString("id").endRecord()
+
+  private val testValSchema: Schema = SchemaBuilder.record("TestRecord")
+    .fields().requiredBoolean("testField").endRecord()
+
+  private def ingest(request: V2IngestRequest): IO[KafkaClientAlgebra[IO]] = for {
+    schemaRegistry <- SchemaRegistry.test[IO]
+    _ <- schemaRegistry.registerSchema(testSubject.value + "-key", testKeySchema)
+    _ <- schemaRegistry.registerSchema(testSubject.value + "-value", testValSchema)
+    kafkaClient <- KafkaClientAlgebra.test[IO]
+    ingestFlow <- IO(new IngestionFlowV2[IO](schemaRegistry, kafkaClient, "https://schemaRegistry.notreal"))
+    _ <- ingestFlow.ingest(request)
+  } yield kafkaClient
+
+  it should "ingest a message" in {
+    val testRequest = V2IngestRequest(testSubject, testKeyPayload, testValPayload.some, ValidationStrategy.Strict)
+    ingest(testRequest).flatMap { kafkaClient =>
+      kafkaClient.consumeMessages(testSubject.value, "test-consumer").take(1).compile.toList.map { publishedMessages =>
+        val firstMessage = publishedMessages.head
+        (firstMessage._1.toString, firstMessage._2.get.toString) shouldBe (testKeyPayload, testValPayload)
+      }
+    }.unsafeRunSync()
+  }
+
+}
