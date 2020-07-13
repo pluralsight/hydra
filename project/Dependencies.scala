83ace43650b50361fb3193df69deece975ac6838
Scala Steward <me@scala-steward.org>
2020-07-13 18:44:40 +0200

    Update avro to 1.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaKafkaStreamVersion = "2.0.3"
   val akkaKryoVersion = "0.5.2"
   val akkaVersion = "2.6.7"
-  val avroVersion = "1.9.2"
+  val avroVersion = "1.10.0"
   val catsEffectVersion = "2.1.4"
   val catsLoggerVersion = "1.1.1"
   val catsRetryVersion = "1.1.1"
