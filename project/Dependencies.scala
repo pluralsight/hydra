107202db78b9ce20b9aa3ea7bcd5de73ccbdcb02
Scala Steward <me@scala-steward.org>
2020-08-26 22:45:29 +0200

    Update kafka-avro-serializer to 5.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val catsRetryVersion = "1.1.1"
   val catsVersion = "2.1.1"
   val cirisVersion = "1.1.2"
-  val confluentVersion = "5.4.2"
+  val confluentVersion = "5.5.1"
   val easyMockVersion = "4.2" //needed for mocking static java methods
   val fs2KafkaVersion = "1.0.0"
   val h2DbVersion = "1.4.200"
