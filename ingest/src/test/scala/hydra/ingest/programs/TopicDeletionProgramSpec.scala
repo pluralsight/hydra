b672dd6b88ff4ebde89c71641edbbce3d356ecb0
Aldrich <william.aldrich.63@gmail.com>
2020-09-04 15:20:57 -0600

    Success cases complete in testing

--- ingest/src/test/scala/hydra/ingest/programs/TopicDeletionProgramSpec.scala
+++ ingest/src/test/scala/hydra/ingest/programs/TopicDeletionProgramSpec.scala
@@ -7,108 +7,50 @@ import hydra.kafka.util.KafkaUtils.TopicDetails
 import org.apache.avro.SchemaBuilder
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import cats.implicits._
 
 class TopicDeletionProgramSpec extends AnyFlatSpec with Matchers {
-  it should "Delete Topics from Kafka value only" in {
+
+  private def applyTestcase(kafkaAdminAlgebra: IO[KafkaAdminAlgebra[IO]],
+                           schemaRegistry: IO[SchemaRegistry[IO]],
+                            topicNames: List[String],
+                            topicNamesToDelete: List[String],
+                            registerKey: Boolean): Unit = {
     (for {
-      kafkaAlgebra <- KafkaAdminAlgebra.test[IO]
-      schemaAlgebra <- SchemaRegistry.test[IO]
-      _ <- kafkaAlgebra.createTopic("test1", TopicDetails(1,1))
-      _ <- schemaAlgebra.registerSchema("test1-value", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(List("test1"))
-      finalTopic <- kafkaAlgebra.describeTopic("test1")
-      finalSchema <- schemaAlgebra.getSchemaFor("test1-value",1)
+      kafkaAlgebra <- kafkaAdminAlgebra
+      schemaAlgebra <- schemaRegistry
+      _ <- topicNames.traverse(topic => kafkaAlgebra.createTopic(topic,TopicDetails(1,1)))
+      _ <- topicNames.flatMap(topic => if(registerKey) List(topic + "-key", topic + "-value") else List(topic + "-value")).traverse(topic =>
+        schemaAlgebra.registerSchema(topic,
+          SchemaBuilder.record("name" + topic.replace("-",""))
+            .fields().requiredString("id" + topic.replace("-","")).endRecord()))
+      _ <-  new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(topicNamesToDelete)
+      allTopics <- kafkaAlgebra.getTopicNames
+      allSchemas <- topicNames.traverse(topic => schemaAlgebra.getAllVersions(topic + "-value").map(versions => if(versions.nonEmpty) Some(topic + "-value") else None)).map(_.flatten)
     } yield {
-      finalTopic shouldBe None
-      finalSchema shouldBe None
+      allTopics shouldBe topicNames.toSet.diff(topicNamesToDelete.toSet).toList
+      allSchemas shouldBe allTopics.map(topic => topic + "-value")
     }).unsafeRunSync()
   }
 
-  it should "Delete Topics from Kafka key and value" in {
-    (for {
-      kafkaAlgebra <- KafkaAdminAlgebra.test[IO]
-      schemaAlgebra <- SchemaRegistry.test[IO]
-      _ <- kafkaAlgebra.createTopic("test1", TopicDetails(1,1))
-      _ <- schemaAlgebra.registerSchema("test1-key", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- schemaAlgebra.registerSchema("test1-value", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(List("test1"))
-      finalTopic <- kafkaAlgebra.describeTopic("test1")
-      finalValueSchema <- schemaAlgebra.getSchemaFor("test1-value",1)
-      finalKeySchema <- schemaAlgebra.getSchemaFor("test1-key",1)
-    } yield {
-      finalTopic shouldBe None
-      finalKeySchema shouldBe None
-      finalValueSchema shouldBe None
-    }).unsafeRunSync()
+  it should "Delete Single Topic from Kafka value only" in {
+    applyTestcase(KafkaAdminAlgebra.test[IO], SchemaRegistry.test[IO], List("topic1"), List("topic1"), registerKey = false)
+  }
+
+  it should "Delete Single Topic from Multiple topics in Kafka value only" in {
+    applyTestcase(KafkaAdminAlgebra.test[IO], SchemaRegistry.test[IO], List("topic1","topic2"), List("topic1"), registerKey = false)
+  }
+
+  it should "Delete Single Topic from Multiple topics in Kafka key and value" in {
+    applyTestcase(KafkaAdminAlgebra.test[IO], SchemaRegistry.test[IO], List("topic1","topic2"), List("topic1"), registerKey = true)
   }
 
   it should "Delete Multiple Topics from Kafka value only" in {
-    (for {
-      kafkaAlgebra <- KafkaAdminAlgebra.test[IO]
-      schemaAlgebra <- SchemaRegistry.test[IO]
-      _ <- kafkaAlgebra.createTopic("test1", TopicDetails(1,1))
-      _ <- kafkaAlgebra.createTopic("test2", TopicDetails(1,1))
-      _ <- schemaAlgebra.registerSchema("test1-value", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- schemaAlgebra.registerSchema("test2-value", SchemaBuilder.record("name2").fields().requiredString("id2").endRecord())
-      _ <- new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(List("test1", "test2"))
-      finalTopicOne <- kafkaAlgebra.describeTopic("test1")
-      finalTopicTwo <- kafkaAlgebra.describeTopic("test2")
-      finalSchemaOne <- schemaAlgebra.getSchemaFor("test1-value",1)
-      finalSchemaTwo <- schemaAlgebra.getSchemaFor("test2-value",1)
-    } yield {
-      finalTopicOne shouldBe None
-      finalSchemaOne shouldBe None
-      finalTopicTwo shouldBe None
-      finalSchemaTwo shouldBe None
-    }).unsafeRunSync()
+    applyTestcase(KafkaAdminAlgebra.test[IO], SchemaRegistry.test[IO], List("topic1", "topic2"), List("topic1","topic2"), registerKey = false)
   }
 
   it should "Delete Multiple Topics from Kafka key and value" in {
-    (for {
-      kafkaAlgebra <- KafkaAdminAlgebra.test[IO]
-      schemaAlgebra <- SchemaRegistry.test[IO]
-      _ <- kafkaAlgebra.createTopic("test1", TopicDetails(1,1))
-      _ <- kafkaAlgebra.createTopic("test2", TopicDetails(1,1))
-      _ <- schemaAlgebra.registerSchema("test1-key", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- schemaAlgebra.registerSchema("test1-value", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- schemaAlgebra.registerSchema("test2-key", SchemaBuilder.record("name2").fields().requiredString("id2").endRecord())
-      _ <- schemaAlgebra.registerSchema("test2-value", SchemaBuilder.record("name2").fields().requiredString("id2").endRecord())
-      _ <- new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(List("test1", "test2"))
-      finalTopicOne <- kafkaAlgebra.describeTopic("test1")
-      finalTopicTwo <- kafkaAlgebra.describeTopic("test2")
-      finalSchemaKeyOne <- schemaAlgebra.getSchemaFor("test1-key",1)
-      finalSchemaKeyTwo <- schemaAlgebra.getSchemaFor("test2-key",1)
-      finalSchemaValueOne <- schemaAlgebra.getSchemaFor("test1-value",1)
-      finalSchemaValueTwo <- schemaAlgebra.getSchemaFor("test2-value",1)
-    } yield {
-      finalTopicOne shouldBe None
-      finalSchemaKeyOne shouldBe None
-      finalSchemaValueOne shouldBe None
-      finalTopicTwo shouldBe None
-      finalSchemaKeyTwo shouldBe None
-      finalSchemaValueTwo shouldBe None
-    }).unsafeRunSync()
-  }
-
-  it should "Delete Only Topic listed from Kafka value only" in {
-    (for {
-      kafkaAlgebra <- KafkaAdminAlgebra.test[IO]
-      schemaAlgebra <- SchemaRegistry.test[IO]
-      _ <- kafkaAlgebra.createTopic("test1", TopicDetails(1,1))
-      _ <- kafkaAlgebra.createTopic("test2", TopicDetails(1,1))
-      _ <- schemaAlgebra.registerSchema("test1-value", SchemaBuilder.record("name").fields().requiredString("id").endRecord())
-      _ <- schemaAlgebra.registerSchema("test2-value", SchemaBuilder.record("name2").fields().requiredString("id2").endRecord())
-      _ <- new TopicDeletionProgram[IO](kafkaAlgebra, schemaAlgebra).deleteTopic(List("test1"))
-      finalTopicOne <- kafkaAlgebra.describeTopic("test1")
-      finalTopicTwo <- kafkaAlgebra.kafkaContainsTopic("test2")
-      finalSchemaOne <- schemaAlgebra.getSchemaFor("test1-value",1)
-      finalSchemaTwo <- schemaAlgebra.getSchemaFor("test2-value",1)
-    } yield {
-      finalTopicOne shouldBe None
-      finalSchemaOne shouldBe None
-      finalTopicTwo shouldBe true
-      finalSchemaTwo shouldBe Some(SchemaBuilder.record("name2").fields().requiredString("id2").endRecord())
-    }).unsafeRunSync()
+    applyTestcase(KafkaAdminAlgebra.test[IO], SchemaRegistry.test[IO], List("topic1", "topic2"), List("topic1","topic2"), registerKey = true)
   }
 
 }
