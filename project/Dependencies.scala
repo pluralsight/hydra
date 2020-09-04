468f1c0e802ac6e011a5be9ebe94a778504859ae
Scala Steward <me@scala-steward.org>
2020-09-04 14:40:43 +0200

    Update ciris to 1.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val catsLoggerVersion = "1.1.1"
   val catsRetryVersion = "1.1.1"
   val catsVersion = "2.1.1"
-  val cirisVersion = "1.2.0"
+  val cirisVersion = "1.2.1"
   val confluentVersion = "5.4.2"
   val easyMockVersion = "4.2" //needed for mocking static java methods
   val fs2KafkaVersion = "1.0.0"
