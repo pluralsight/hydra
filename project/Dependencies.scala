94ab29f345fc4b2aab1c53ce57c374ae8389f5d1
Scala Steward <me@scala-steward.org>
2020-12-17 00:51:49 +0100

    Update scalatest to 3.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val reflectionsVersion = "0.9.12"
   val scalaCacheVersion = "0.28.0"
   val scalaMockVersion = "5.1.0"
-  val scalaTestVersion = "3.2.2"
+  val scalaTestVersion = "3.2.3"
   val scalazVersion = "7.3.2"
   val sprayJsonVersion = "1.3.5"
   val testContainersVersion = "0.38.7"
