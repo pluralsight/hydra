2d84a4ea1ab77e3dc45bfea8e5e67ba727a594e3
Monte Misa <montemisa@gmail.com>
2021-01-13 14:33:21 -0700

    Use subject.value instead of subject.toString

--- ingestors/kafka/src/main/scala/hydra/kafka/algebras/MetadataAlgebra.scala
+++ ingestors/kafka/src/main/scala/hydra/kafka/algebras/MetadataAlgebra.scala
@@ -36,7 +36,7 @@ object MetadataAlgebra {
                                             consumeMetadataEnabled: Boolean
                                           ): F[MetadataAlgebra[F]] = {
     val metadataStream: fs2.Stream[F, (GenericRecord, Option[GenericRecord])] = if (consumeMetadataEnabled) {
-      kafkaClientAlgebra.consumeMessages(metadataTopicName.toString, consumerGroup, commitOffsets = false).map(record => (record._1, record._2))
+      kafkaClientAlgebra.consumeMessages(metadataTopicName.value, consumerGroup, commitOffsets = false).map(record => (record._1, record._2))
     } else {
       fs2.Stream.empty
     }
