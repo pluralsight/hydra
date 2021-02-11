6129874b04595ef12e6fb00efc6110462d834e4c
Scala Steward <me@scala-steward.org>
2021-02-11 21:28:29 +0100

    Update database-commons, jdbc, ... to 1.15.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -153,7 +153,7 @@ object Dependencies {
   }
 
   object Integration {
-    private val testcontainersJavaVersion = "1.15.1"
+    private val testcontainersJavaVersion = "1.15.2"
     val testContainers = Seq(
       "com.dimafeng" %% "testcontainers-scala-scalatest" % testContainersVersion % "it",
       "com.dimafeng" %% "testcontainers-scala-kafka" % testContainersVersion % "it",
