409cec8ed62198e3b5d54643485e1791f5eefbb6
Aldrich <william.aldrich.63@gmail.com>
2021-04-02 11:11:05 -0600

    add retry logic

--- ingestors/kafka/src/test/scala/hydra/kafka/algebras/TagsAlgebraSpec.scala
+++ ingestors/kafka/src/test/scala/hydra/kafka/algebras/TagsAlgebraSpec.scala
@@ -1,28 +1,44 @@
 package hydra.kafka.algebras
 
-import cats.effect.{Concurrent, ContextShift, IO, Sync}
+import cats.effect.{Concurrent, ContextShift, IO, Sync, Timer}
+import cats.implicits._
 import hydra.kafka.algebras.KafkaClientAlgebra.PublishResponse
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
+import retry.RetryPolicies.{exponentialBackoff, limitRetries}
+import retry.{RetryDetails, RetryPolicy}
+import retry.syntax.all._
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 class TagsAlgebraSpec extends AnyWordSpecLike with Matchers {
   implicit private val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
   private implicit val concurrentEffect: Concurrent[IO] = IO.ioConcurrentEffect
+
+  private implicit val policy: RetryPolicy[IO] = limitRetries[IO](5) |+| exponentialBackoff[IO](500.milliseconds)
+  private implicit val timer: Timer[IO] = IO.timer(ExecutionContext.global)
+  private implicit def noop[A]: (A, RetryDetails) => IO[Unit] = retry.noop[IO, A]
+
   implicit private def unsafeLogger[F[_]: Sync]: SelfAwareStructuredLogger[F] =
     Slf4jLogger.getLogger[F]
+
+  private implicit class RetryAndAssert[A](boolIO: IO[A]) {
+    def retryIfFalse(check: A => Boolean): IO[Assertion] =
+      boolIO.map(check).retryingOnFailures(identity, policy, noop).map(assert(_))
+  }
 
   (for {
     kafkaClientAlgebra <- KafkaClientAlgebra.test[IO]
     tagsAlgebra <- TagsAlgebra.make("_dvs.tags-topic", "tagsClient", kafkaClientAlgebra)
-  } yield (runTests(tagsAlgebra, kafkaClientAlgebra))
+  } yield runTests(tagsAlgebra)
   ).unsafeRunSync()
 
 
-  private def runTests(tagsAlgebra: TagsAlgebra[IO], kafkaClientAlgebra: KafkaClientAlgebra[IO]): Unit = {
+  private def runTests(tagsAlgebra: TagsAlgebra[IO]): Unit = {
     "TagsAlgebraSpec" should {
       "Create a Tag" in {
         val tag = HydraTag("Source: blah", "This comes from blah")
@@ -38,6 +54,9 @@ class TagsAlgebraSpec extends AnyWordSpecLike with Matchers {
         val tag = HydraTag("Source: Bret", "This comes from Bret being on a call while I do this")
         tagsAlgebra.createOrUpdateTag(tag)
           .unsafeRunSync shouldBe Right(PublishResponse(0,2))
+      }
+      "Get Tags" in {
+        tagsAlgebra.getAllTags.retryIfFalse(_.size == 2)
       }
       "return true when validating a good tag" in {
         tagsAlgebra.validateTags(List("Source: blah")).unsafeRunSync() shouldBe ()
