c2b491dc0e2ba2390e57632ae9fd4ace273906be
znickle24 <znickle24@icloud.com>
2021-06-11 11:38:52 -0600

    Updating timeout for schemas endpoint.

--- ingest/src/main/scala/hydra.ingest/http/SchemasEndpoint.scala
+++ ingest/src/main/scala/hydra.ingest/http/SchemasEndpoint.scala
@@ -70,7 +70,7 @@ class SchemasEndpoint(consumerProxy: ActorSelection)(implicit system: ActorSyste
     val make: List[SchemasWithTopicResponse] => BatchSchemasResponse = BatchSchemasResponse.apply
     jsonFormat1(make)
   }
-  implicit val timeout: Timeout = Timeout(10.seconds)
+  implicit val timeout: Timeout = Timeout(3.seconds)
 
   private val schemaRegistryActor =
     system.actorOf(SchemaRegistryActor.props(applicationConfig))
