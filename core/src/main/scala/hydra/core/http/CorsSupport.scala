a97fbfe56a4670419faf72bdd758748e47df6292
Zachary Moix <zacharymoix@Zacharys-MacBook-Pro.local>
2021-10-18 10:25:27 -0600

    adding cors configuration for *.vnerd.com

--- core/src/main/scala/hydra/core/http/CorsSupport.scala
+++ core/src/main/scala/hydra/core/http/CorsSupport.scala
@@ -3,6 +3,9 @@ package hydra.core.http
 /**
   * Created by alexsilva on 3/28/17.
   */
+import akka.http.scaladsl.model.headers.{Host, HttpOrigin}
+import akka.http.scaladsl.model.{HttpMethod, HttpMethods, Uri}
+import ch.megard.akka.http.cors.scaladsl.model.HttpOriginMatcher
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import hydra.common.config.ConfigSupport
 
@@ -10,7 +13,14 @@ import scala.collection.immutable
 
 trait CorsSupport extends ConfigSupport {
 
+  private val allowedMethodKeys = scala.collection.immutable.Seq("GET", "POST", "PUT", "HEAD", "OPTIONS", "DELETE")
+  private val allowedMethods =  allowedMethodKeys.map(k => HttpMethods.getForKey(k).getOrElse(HttpMethod.custom(k)))
+
+  private val dataExplorerWildcardString = "*.vnerd.com"
+
   val settings = CorsSettings.defaultSettings
     .withAllowCredentials(false)
     .withExposedHeaders(immutable.Seq("Link"))
+    .withAllowedMethods(allowedMethods)
+    .withAllowedOrigins(HttpOriginMatcher.apply(HttpOrigin(dataExplorerWildcardString)))
 }
