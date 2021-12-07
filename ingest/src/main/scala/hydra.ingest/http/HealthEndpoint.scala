511c6c1472a4f62ba97f1008bea73e1070316f27
Aldrich <william.aldrich.63@gmail.com>
2021-12-07 09:22:35 -0700

    add whitespace, need to remove, testing build

--- ingest/src/main/scala/hydra.ingest/http/HealthEndpoint.scala
+++ ingest/src/main/scala/hydra.ingest/http/HealthEndpoint.scala
@@ -33,10 +33,12 @@ class HealthEndpoint[F[_] : Futurable](cga: ConsumerGroupsAlgebra[F]) extends Ro
                     addHttpMetric("",StatusCodes.InternalServerError,"/health", startTime, method.value, error = Some(exception.getMessage))
                     complete(StatusCodes.InternalServerError, BuildInfo.toJson + exception.getMessage)
                   case Success(value) =>
+
                     if(value._1) {
                       addHttpMetric("", StatusCodes.OK, "/health", startTime, method.value)
                       complete(StatusCodes.OK, addConsumerGroupStateToBuild(BuildInfo.toJson, value._2, value._1))
                     } else {
+
                       addHttpMetric("", StatusCodes.InternalServerError, "/health", startTime, method.value)
                       complete(StatusCodes.InternalServerError, addConsumerGroupStateToBuild(BuildInfo.toJson, value._2, value._1))
                     }
