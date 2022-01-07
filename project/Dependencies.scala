fc80e195484065d7695e934aea26d40a88b77147
Scala Steward <me@scala-steward.org>
2022-01-07 22:45:45 +0100

    Update fs2-kafka to 1.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val catsVersion = "2.2.0"
   val cirisVersion = "1.2.1"
   val confluentVersion = "5.4.2"
-  val fs2KafkaVersion = "1.0.0"
+  val fs2KafkaVersion = "1.9.0"
   val jacksonCoreVersion = "2.10.4"
   val jacksonDatabindVersion = "2.10.4"
   val jodaConvertVersion = "2.2.1"
